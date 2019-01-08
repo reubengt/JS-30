03d30d80fb2f25c9faf8d798686b641c9adccbc1
reubengt <reubengeorgethomas@gmail.com>
2019-01-08 21:33:16 +0000

    add minute hand and hour hand motion using setInterval

--- clock/app.js
+++ clock/app.js
@@ -1,8 +1,17 @@
 const secondHand=document.querySelector(".second-hand");
+const minHand=document.querySelector(".min-hand");
+const hourHand=document.querySelector(".hour-hand");
 function setDate(){
   const now = new Date();
   const seconds = now.getSeconds();
-  const secondsDegrees = (seconds*6)+90;
+  const mins = now.getMinutes();
+  const hours=now.getHours();
+  const secondsDegrees = (seconds/60*360)+90;
   secondHand.style.transform=`rotate(${secondsDegrees}deg)`;
+  const minDegrees = (mins/60*360)+90;
+  minHand.style.transform=`rotate(${minDegrees}deg)`;
+  const hourDegrees = (hours/60*360)+90;
+  hourHand.style.transform=`rotate(${hourDegrees}deg)`;
 }
+setDate();
 setInterval(setDate, 1000);
