398ca01a424b2a24e32ba738486f9b8666212cf6
reubengt <reubengeorgethomas@gmail.com>
2019-01-08 13:11:30 +0000

    fix audio repeat issue when key is held down

--- drumkit/app.js
+++ drumkit/app.js
@@ -2,9 +2,12 @@ window.addEventListener('keydown',function(e){
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
   if(!audio) return;
+  if(key.classList.contains('playing')==false)
+  {
   audio.currentTime=0;
   audio.play();
   key.classList.add('playing');
+  }
 });
 window.addEventListener('keyup',function(e){
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
