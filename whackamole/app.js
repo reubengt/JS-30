6e978f9f79ec1bc79a72bd14292d0d8e6903e6df
reubengt <reubengeorgethomas@gmail.com>
2019-02-03 21:45:28 +0000

    add html css js files and images and sounds for whackamole game

---
+++ whackamole/app.js
@@ -0,0 +1,46 @@
+const holes = document.querySelectorAll('.hole');
+const scoreBoard = document.querySelector('.score');
+const moles = document.querySelectorAll('.mole');
+const startButton = document.querySelector('button');
+const squeal = document.querySelector('audio');
+let lastHole;
+let timeUp = false;
+let score = 0;
+function randomTime(min, max) {
+   return Math.round(Math.random() * (max - min) + min);
+ }
+ function randomHole(holes) {
+   const idx = Math.floor(Math.random() * holes.length);
+   const hole = holes[idx];
+   if (hole === lastHole) {
+     return randomHole(holes);
+   }
+   lastHole = hole;
+   return hole;
+ }
+ function peep() {
+   const time = randomTime(200, 1000);
+   const hole = randomHole(holes);
+   hole.classList.add('up');
+   setTimeout(() => {
+     hole.classList.remove('up');
+     if (!timeUp) peep();
+   }, time);
+ }
+ function startGame() {
+   scoreBoard.textContent = 0;
+   timeUp = false;
+   score = 0;
+   peep();
+   setTimeout(() => {timeUp = true; alert("TIME OVER")}, 30000)
+ }
+ function bonk(e) {
+   if(!e.isTrusted) return; // cheater!
+   squeal.currentTime=0;
+   squeal.play();
+   score++;
+   this.parentNode.classList.remove('up');
+   scoreBoard.textContent = score;
+ }
+  moles.forEach(mole => mole.addEventListener('click', bonk));
+  startButton.addEventListener('click', startGame);
